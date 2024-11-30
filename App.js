1b2694978f9a218755333fadf0e18caad13a8d04
meghana <meghanashree.rk@mresult.com>
2024-11-30 12:23:53 +0530

    map function

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React functional component
-
 const Header = () => {
   return (
     <div className="header">
@@ -28,27 +26,28 @@ const stylecard = { backgroundColor: " white " }; //css as an object used for in
 
 const RestaurentCard = (props) => {
   const { resData } = props;
-  console.log(props);
+  const { name, count, cloudinaryImageId, rating, location } = resData;
   return (
     <div className="res-card" style={stylecard}>
       <img
         src={
           "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          resData.cloudinaryImageId
+          cloudinaryImageId
         }
         className="res-logo"
         alt="res-logo"
       />
-      <h3>{resData.name}</h3>
-      <h4>{resData.count}</h4>
-      <h4>{resData.rating}</h4>
-      <h4>{resData.location}</h4>
+      <h3>{name}</h3>
+      <h4>{count}</h4>
+      <h4>{rating}</h4>
+      <h4>{location}</h4>
     </div>
   );
 };
 
 const resList = [
   {
+    id: 0,
     name: "Coffee Shop A",
     count: 5,
     rating: 4.7,
@@ -56,6 +55,7 @@ const resList = [
     cloudinaryImageId: "hjao7sorzggaeqito6au", // these cloudinary image ids are taken from swiggy website, it is unique for each image, the cdn url remains the same
   },
   {
+    id: 1,
     name: "Bookstore B",
     count: 3,
     rating: 4.9,
@@ -63,6 +63,7 @@ const resList = [
     cloudinaryImageId: "wa8ek94fp9d97ru1tcyn",
   },
   {
+    id: 2,
     name: "Restaurant C",
     count: 10,
     rating: 4.5,
@@ -70,6 +71,7 @@ const resList = [
     cloudinaryImageId: "vkhcohhmqfczycw9vsar",
   },
   {
+    id: 3,
     name: "Gym D",
     count: 7,
     rating: 4.8,
@@ -77,6 +79,7 @@ const resList = [
     cloudinaryImageId: "zvy2c2xcxzzlxg6tifvn",
   },
   {
+    id: 4,
     name: "Salon E",
     count: 2,
     rating: 4.6,
@@ -90,11 +93,11 @@ const Body = (props) => {
     <div className="body">
       <div className="search">Search </div>
       <div className="res-container">
-        <RestaurentCard resData={resList[0]} />
-        <RestaurentCard resData={resList[1]} />
-        <RestaurentCard resData={resList[2]} />
-        <RestaurentCard resData={resList[3]} />
-        <RestaurentCard resData={resList[4]} />
+        {resList.map((restaurant) => (
+          // you can also use index as key but it snot recommended, bad practice. But,
+          // not using key <<< index <<< unique key (best practice).
+          <RestaurentCard key={restaurant.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
